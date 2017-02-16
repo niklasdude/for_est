8aa6d5b067c5f1c9174be7d27a4954206c87ac2b
niklasdude <dauteniklas@googlemail.com>
2017-02-16 15:04:28 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -4,13 +4,14 @@
 function main() {
 
 $('.body').hide();
-$('.body').fadeIn(1000);  
+$('.body').fadeIn(1000);
 
 
-
-
-
-
-}
-
+jQuery({ Counter: 0 }).animate({ Counter: $('.Single').text() }, {
+	  duration: 1000,
+	  easing: 'swing',
+	  step: function () {
+	    $('.Single').text(Math.ceil(this.Counter));
+	  }
+	});
 $(document).ready(main);
