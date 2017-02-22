641a897463167bd4a40f7418e0abee7cbe15d18d
niklasdude <dauteniklas@googlemail.com>
2017-02-22 15:56:54 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -7,22 +7,56 @@ function main() {
 $(shots).hide();
 $('.body').hide();
 $('.body').fadeIn(1000);
-
+$('.lipro').hover(function(){
+	$('#probar').css('top','6%');
+	$('#probar').css('background-color','#fff')
+});
 }
 
+
+
+$(".lipro").hover(function(){
+    $('#probar').css("display", "block");
+    });
+/**$('#probar').hover(function{
+	$('#probar').css("display", "block");
+}, function(){
+	$('#probar').css('display', 'none');
+});*/
+$('div').hover(function(){
+	$('#probar').css('display', 'none');
+});
+
+
+
+$('input').focus(function(){
+    $('input').css('outline-color','#FF0000');   
+});
+
+$('input').keydown(function(){
+	$('input').css('background-color','rgba(89, 255, 125,0.7)');
+}); 
+var value = $('#namex').val();
+if( value == "YOUR NAME"){
+	$('#namex').css('background-color','rgba(0,0,0,0.3)');	
+};
 
 jQuery({ Counter: 0 }).animate({ Counter: $('.Single').text() }, {
 	  duration: 1000,
 	  easing: 'swing',
 	  step: function () {
-	    $('.Single').text(Math.ceil(this.Counter));
+	  $('.Single').text(Math.ceil(this.Counter));
 	  }
 	});
 $(document).ready(main);
+
 function gradient(){
 	$("namex").css("background-color","blue");
 }
+
 document.getElementById("testgame").onclick = function() {myFunction()};
+
+
 function myFunction() {
 	
 	$(gamestable).fadeOut(500);
@@ -30,4 +64,4 @@ function myFunction() {
 	
 	
 		/*alert("Hello! I am an alert box!!");*/
-	}
+}
